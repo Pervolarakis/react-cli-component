838c6a6bd9b798345c1fd77bdc361ce9f3a61c5f
Your Name <mihalaras11@gmail.com>
2020-10-17 16:01:28 +0300

    Updates

---
+++ precache-manifest.a9bd459a576a700847e0e3eead79efd3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0adc6ec42029d1085ae52bfb043792b4",
+    "url": "/react-cli-component/index.html"
+  },
+  {
+    "revision": "6489b85be3c640a95c99",
+    "url": "/react-cli-component/static/css/main.7d29222f.chunk.css"
+  },
+  {
+    "revision": "f2528d83ea2470973c83",
+    "url": "/react-cli-component/static/js/2.77fabf10.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-cli-component/static/js/2.77fabf10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6489b85be3c640a95c99",
+    "url": "/react-cli-component/static/js/main.0e964e69.chunk.js"
+  },
+  {
+    "revision": "fa6892f01edd7deed003",
+    "url": "/react-cli-component/static/js/runtime-main.ec8380e7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-cli-component/static/media/logo.5d5d9eef.svg"
+  }
+]);
